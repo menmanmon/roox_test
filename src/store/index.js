1b0a078b487c4da449c4c62f4a56db19da207b75
manmanmon <burgoch-roman9a@mail.ru>
2022-03-28 11:22:35 +0300

    deleted all files except dist folder for hosting on github pages

--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-import { usersReducer } from "./users/reducer";
-
-export const store = createStore(
-  usersReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
